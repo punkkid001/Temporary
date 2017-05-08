1f17aef545825070408d897b2843d7949845b665
punkyoon <cryptography@konkuk.ac.kr>
2017-05-08 20:38:43 +0900

    file transfer

--- ErrorControlClient.java
+++ ErrorControlClient.java
@@ -7,37 +7,68 @@ class ErrorControlClient
     public static void main(String[] args)throws IOException
     {
         Scanner input = new Scanner(System.in);
-        String name, ip = "127.0.0.1";
-        int selection = 0, port;
-
         Socket socket;
+        
+        InputStream in;
+        DataInputStream d_in;
+        OutputStream out;
+        DataOutputStream d_out;
+
+        String ip = "127.0.0.1";
+        int selection = 0, port = 0;
 
         System.out.print("1) Stop and wait   2) Go Back N : ");
         selection = input.nextInt();
-
-        System.out.print("File name : ");
-        name = input.next();
 
         if (selection==1)
         {
             System.out.println("=====Stop and Wait=====");
             port = 8000;
-            socket = new Socket(ip,port);
-
-            System.out.println("Connect!");
-            System.out.println();
         }
         else if (selection==2)
         {
             System.out.println("=====Go Back N=====");
             port = 8001;
-            socket = new Socket(ip,port);
-
-            System.out.println("Connect!");
-            System.out.println();
         }
         else
+        {
             System.out.println("Wrong selection!");
+            System.exit(-1);
+        }
+        
+        socket = new Socket(ip,port);
+
+        System.out.println("Connect!");
+        System.out.println();
+
+        in=socket.getInputStream();
+        d_in=new DataInputStream(in);
+        out=socket.getOutputStream();
+        d_out=new DataOutputStream(out);
+
+        BufferedOutputStream bos=new BufferedOutputStream(socket.getOutputStream());
+        DataOutputStream dos=new DataOutputStream(bos);
+
+        FileOutputStream f_out=new FileOutputStream("result");
+        while(true)
+        {
+            int data=d_in.read();
+            if(data==-1)
+            {
+                break;
+            }
+            f_out.write(data);
+        }
+
+        System.out.println("Done!");
+
+        //Closing socket
+        socket.close();
+        in.close();
+        d_in.close();
+        out.close();
+        d_out.close();
+        f_out.close();
     }
 
     public static void stop_and_wait()
--- ErrorControlServer.java
+++ ErrorControlServer.java
@@ -7,38 +7,77 @@ class ErrorControlServer
     public static void main(String[] args)throws IOException
     {
         Scanner input = new Scanner(System.in);
-        String ip = "127.0.0.1";
-        int selection = 0, port;
-
-        //Making Socket
+        BufferedReader file;
         ServerSocket server_s;
         Socket socket;
 
+        InputStream in;
+        DataInputStream d_in;
+        OutputStream out;
+        DataOutputStream d_out;
+        DataOutputStream d_file_out;
+
+        String name, ip = "127.0.0.1";
+        int selection = 0, port = 0;
+
         System.out.print("1) Stop and wait   2) Go Back N : ");
         selection = input.nextInt();
+
+        System.out.print("File name : ");
+        name = input.next();
+
+        file = new BufferedReader(new FileReader(name));
 
         if (selection==1)
         {
             System.out.println("=====Stop and Wait=====");
             port = 8000;
-            server_s=new ServerSocket(port);
-
-            socket=server_s.accept();
-            System.out.println("Connect!");
-
         }
         else if (selection==2)
         {
             System.out.println("=====Go Back N=====");
             port = 8001;
-            server_s=new ServerSocket(port);
-            
-            socket=server_s.accept();
-            System.out.println("Connect!");
-
         }
         else
+        {
             System.out.println("Wrong selection!");
+            System.exit(-1);
+        }
+        
+        server_s=new ServerSocket(port);
+
+        socket=server_s.accept();
+        System.out.println("Connect!");
+        
+        in=socket.getInputStream();
+        d_in=new DataInputStream(in);
+        out=socket.getOutputStream();
+        d_out=new DataOutputStream(out);
+        d_file_out=new DataOutputStream(out);
+
+        FileInputStream f_in=new FileInputStream(name);
+        System.out.println("Transmitting ..");
+        
+        while(true)
+        {
+            int data=f_in.read();
+            if(data==-1)
+            {
+                break;
+            }
+            d_out.write(data);
+        }
+
+        System.out.println("Done!");
+
+        //Closing Socket
+        server_s.close();
+        socket.close();
+        in.close();
+        d_in.close();
+        out.close();
+        d_out.close();
+        f_in.close();
     }
 
     public static void stop_and_wait()
